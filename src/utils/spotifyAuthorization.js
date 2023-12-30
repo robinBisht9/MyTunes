2af69b3b845ef2196f2ee47b796c995c1964a998
robinBisht <robinbisht34@gmail.com>
2023-12-30 21:58:45 +0530

    Adding env variables

--- src/utils/spotifyAuthorization.js
+++ src/utils/spotifyAuthorization.js
@@ -2,7 +2,6 @@ export const CLIENT_ID = "41d0e0e9bff84790b27d66a016337964";
 export const CLIENT_SECRET_ID = "447623795a8249119de35d07f390e513";
 export const AUTH_URL = "https://accounts.spotify.com/authorize";
 export const AUTH_TOKEN_URL = "https://accounts.spotify.com/api/token";
-export const REDIRECT_URI = "http://localhost:5173/spotifyCallback";
 
 const scopes = [
   "user-read-private",
@@ -15,6 +14,6 @@ const scopes = [
 
 export const spotifyAuthUrl = `${AUTH_URL}?
 client_id=${CLIENT_ID}
-&redirect_uri=${REDIRECT_URI}
+&redirect_uri=${import.meta.env.VITE_SPOTIFY_CALLBACK_URL}
 &response_type=token
 &scope=${scopes.join("%20")}`;
